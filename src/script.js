753ef40af1aaa15df794708a5b4584e4bbaa0da3
Bartosz Lorek <bery.lorek@gmail.com>
2017-04-02 13:06:56 +0200

    clean code and do tests

--- src/script.js
+++ src/script.js
@@ -1,106 +1,6 @@
 import CASE_METHODS from './cases';
 import dispatchError from './error';
-
-function nextNode(node) {
-    if (node.hasChildNodes()) {
-        return node.firstChild;
-    } else {
-        while (node && !node.nextSibling)
-            node = node.parentNode;
-        return node ? node.nextSibling : null;
-    }
-}
-
-function rangeTextNodes(range) {
-    let node = range && range.start || null,
-        endNode = range && range.end || null,
-        nodes = [];
-
-    if (!node || !endNode) return nodes;
-    if (node === endNode) return [node];
-    if (node.nodeType === 3) {
-        nodes.push(node);
-    }
-    while (node && node !== endNode) {
-        node = nextNode(node);
-        if (node.nodeType === 3) {
-            nodes.push(node);
-        }
-    }
-    return nodes;
-}
-
-function parseNodes(range) {
-    if (range === false) {
-        return [];
-    }
-    let nodes = rangeTextNodes(range),
-        nodesLength = nodes.length,
-        output = [];
-
-    for (let i=0; i<nodesLength; i++) {
-        let node = nodes[i],
-            text = node.nodeType === 3
-                 ? node.nodeValue : node.value,
-            start = i == 0 ? range.offset[0] : 0,
-            stop = i == nodesLength - 1
-                 ? range.offset[1] : text.length;
-        output.push({
-            element: node,
-            range: [start, stop],
-            text: text
-        })
-    }
-    return output;
-}
-
-function selectionRange(localWindow, localDocument) {
-    localWindow = localWindow || window;
-    localDocument = localDocument || document;
-
-    let element = localDocument.activeElement,
-        tagName = element.tagName && element.tagName.toLowerCase() || false,
-        selection,
-        range;
-
-    if (tagName === false) return false;
-    if (tagName === 'textarea' || tagName === 'input') {
-        try {
-            return {
-                empty: () => localWindow.getSelection().empty(),
-                start: element,
-                end: element,
-                offset: [
-                    element.selectionStart,
-                    element.selectionEnd
-                ]
-            }
-        } catch(e) {
-            return false;
-        }
-    }
-    if (tagName === 'iframe' || tagName === 'frame') {
-        return selectionRange(
-            element.contentWindow,
-            element.contentDocument
-        )
-    }
-    selection = localWindow.getSelection();
-    if (selection.rangeCount === 0) {
-        return false;
-    }
-    range = selection.getRangeAt(0);
-    
-    return {
-        empty: () => selection.empty(),
-        start: range.startContainer,
-        end: range.endContainer,
-        offset: [
-            range.startOffset,
-            range.endOffset
-        ]
-    }
-}
+import selectionText from './selection';
 
 function dispatchEvents(element) {
     if (element.nodeType !== 1) {
@@ -143,27 +43,20 @@ if (typeof chrome !== 'undefined') {
             return false;
         }
         let methodName = request.value || false,
-            range = selectionRange(),
-            nodes;
-
-        if (range === false) {
+            selection = selectionText();
+        if (selection.length === 0) {
             dispatchError();
             return false;
         }
-        range.empty();
-        nodes = parseNodes(range);
-        for (let i=0; i<nodes.length; i++) {
-            changeCase(methodName, nodes[i]);
+        for (let i=0; i<selection.length; i++) {
+            changeCase(methodName, selection[i]);
         }
     })
 }
 
-export let testExport;
+export let _private;
 if (process && process.env.NODE_ENV === 'test') {
-    testExport = {
-        nextNode,
-        rangeTextNodes,
-        parseNodes,
+    _private = {
         changeCase
     }
 }
--- test/script.test.js
+++ test/script.test.js
@@ -1,91 +1,17 @@
-import { testExport } from '../src/script.js';
-const { nextNode, rangeTextNodes, parseNodes, changeCase } = testExport;
+import { _private } from '../src/script.js';
+const { changeCase } = _private;
 
 describe('script.js', () => {
+    document.body.innerHTML = '<p id="element">text to change</p>';
 
-    document.body.innerHTML =
-        '<p id="first">the quick brown</p>' +
-        '<p id="second">fox jumps over</p>' +
-        '<p id="third">the lazy dog</p>';
-
-    let fakeRange = {
-        empty: () => {},
-        start: document.getElementById('first').firstChild,
-        end: document.getElementById('second').firstChild,
-        offset: [2, 11]
-    }
-
-    it('fake range is working', () => {
-        expect(
-            fakeRange.start.nodeValue
-        ).toEqual('the quick brown')
-    })
-    
-    describe('nextNode', () => {
-        let first = nextNode(fakeRange.start.parentElement),
-            second = nextNode(first),
-            third = nextNode(second);
-
-        it('should return first text node', () => {
-            expect(
-                first.nodeValue
-            ).toEqual('the quick brown')
-        })
-        it('should return second paragraph', () => {
-            expect(
-                second.id
-            ).toEqual('second')
-        })
-        it('should return second text node', () => {
-            expect(
-                third.nodeValue
-            ).toEqual('fox jumps over')
-        })
-    })
-
-    describe('rangeTextNodes', () => {
-        it('should return empty array', () => {
-            expect(
-                rangeTextNodes(null)
-            ).toEqual([]);
-        })
-        it('should return array', () => {
-            expect(
-                rangeTextNodes(fakeRange)
-            ).toBeInstanceOf(Array);
-        })
-        it('should return 2 text node', () => {
-            expect(
-                rangeTextNodes(fakeRange).length
-            ).toEqual(2);
-        })
-        it('first item should be first paragraph text node', () => {
-            expect(
-                rangeTextNodes(fakeRange)[0].nodeValue
-            ).toEqual('the quick brown');
-        })
-    })
-
-    describe('parseNodes', () => {
-        it('should return empty array', () => {
-            expect(
-                parseNodes(null)
-            ).toEqual([]);
-        })
-        it('first item`s should be [2,15]', () => {
-            expect(
-                parseNodes(fakeRange)[0].range
-            ).toEqual([2, 15]);
-        })
-    })
-    
     describe('changeCase', () => {
-        let fakeNode = {
-            element: document.getElementById('third').firstChild,
-            range: [2, 12],
-            text: 'text to change',
-            type: 'text'
-        }
+        let element = document.getElementById('element'),
+            fakeNode = {
+                element: element.firstChild,
+                text: element.textContent,
+                range: [2, 12],
+                type: 'text'
+            }
         
         it('should return false', () => {
             expect(
