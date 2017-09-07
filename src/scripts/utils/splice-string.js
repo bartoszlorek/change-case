3ab374c22a0ddce00e0ba0afdcd3bbb38b0e515a
Bartosz Lorek <bery.lorek@gmail.com>
2017-09-07 17:19:50 +0200

    clean code

--- src/scripts/utils/splice-string.js
+++ src/scripts/utils/splice-string.js
@@ -1,12 +1,15 @@
-function spliceString(string, index, count, replace) {
+function spliceString(string, index, count, replacement) {
     if (index < 0) {
         index = string.length + index;
         if (index < 0) {
             index = 0;
         }
     }
-    return string.slice(0, index) + (replace || '') +
-        string.slice(index + count);
+    return (
+        string.slice(0, index) +
+        replacement || '' +
+        string.slice(index + count)
+    )
 }
 
 export default spliceString;
