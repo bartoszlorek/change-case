4e323169d8d112c1311f5a5ff5cc0888bfceadb0
Bartosz Lorek <bery.lorek@gmail.com>
2017-09-14 00:37:45 +0200

    clean code

--- src/scripts/utils/node-value.js
+++ src/scripts/utils/node-value.js
@@ -3,14 +3,14 @@ import isTextElement from './is-text-element';
 
 function nodeValue(node, value) {
     if (isTextNode(node)) {
-        return typeof value === 'string' ?
-            node.nodeValue = value :
-            node.nodeValue;
+        return typeof value === 'string'
+            ? node.nodeValue = value
+            : node.nodeValue;
     }
     if (isTextElement(node)) {
-        return typeof value === 'string' ?
-            node.value = value :
-            node.value;
+        return typeof value === 'string'
+            ? node.value = value
+            : node.value;
     }
     return '';
 }
