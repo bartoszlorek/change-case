40ab6204a12b807fc63f7d97737e2dcec30dbd0c
Bartosz Lorek <bery.lorek@gmail.com>
2017-09-15 02:38:12 +0200

    Update apply-method.js

--- src/scripts/apply-method.js
+++ src/scripts/apply-method.js
@@ -2,20 +2,19 @@ import CASE_METHODS from './cases';
 import nodeValue from './utils/node-value';
 import spliceString from './utils/splice-string';
 
-function applyMethod(methodName, selected, filter) {
+function applyMethod(methodName, element, filter) {
     let method = CASE_METHODS[methodName],
         result;
 
-    if (typeof method !== 'function' || !selected) {
+    if (typeof method !== 'function' || element == null) {
         return Promise.reject();
     }
     if (typeof filter === 'function') {
         method = filter(method);
     }
-
     return Promise.resolve(method).then(resolved => {
-        let { text, startOffset, endOffset } = selected;
-        return nodeValue(selected.node, spliceString(
+        let { text, startOffset, endOffset } = element;
+        return nodeValue(element.node, spliceString(
             text, startOffset, endOffset, resolved));
     });
 }
