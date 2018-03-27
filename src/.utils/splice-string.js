93141158b696b9ef1e16895a21799aa1aea49de0
Bartosz Lorek <bery.lorek@gmail.com>
2018-03-28 00:13:08 +0200

    assign length to end when switching with replacement

--- src/.utils/splice-string.js
+++ src/.utils/splice-string.js
@@ -2,21 +2,25 @@ function spliceString(string, start, end, replacement) {
     if (typeof string !== 'string') {
         return ''
     }
+    let length = string.length
+
     if (replacement === undefined && typeof end === 'function') {
         replacement = end
         end = length
     }
-    start = index(start, string.length, 0)
-    end = index(end, string.length, string.length)
+    start = index(start, length, 0)
+    end = index(end, length, length)
 
     if (start > end) {
         end = [start, (start = end)][0]
     }
-    if (start > string.length) {
+    if (start > length) {
         return string
     }
+
     if (typeof replacement === 'function') {
-        replacement = replacement(string.substring(start, end))
+        replacement = replacement(string
+            .substring(start, end))
     }
     return (
         string.substring(0, start)
