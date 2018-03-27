0abff5bae1bf5b223feadc6c720e878bde6c4cc8
Bartosz Lorek <bery.lorek@gmail.com>
2018-03-28 00:14:26 +0200

    return empty array for falsy inputs

--- src/.utils/indices-of.js
+++ src/.utils/indices-of.js
@@ -1,8 +1,11 @@
-function indicesOf(string, value) {
+function indicesOf(string = '', value = '') {
     let startIndex = 0,
         indices = [],
         index
 
+    if (!string || !value) {
+        return []
+    }
     while ((index = string.indexOf(value, startIndex)) > -1) {
         indices.push(index)
         startIndex = index + value.length
