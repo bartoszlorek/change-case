e511a98e409986a28938657bf8f36e7adeba0e87
ubery <bery.lorek@gmail.com>
2018-04-02 00:58:08 +0200

    remove semicolons

--- src/.utils/react/apply-markdown.js
+++ src/.utils/react/apply-markdown.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import markdown from './markdown';
+import React from 'react'
+import markdown from './markdown'
 
 function applyMarkdown(style, spec) {
     if (style == null || spec == null) {
-        return (string) => string;
+        return string => string
     }
-    let marks = Object.keys(spec);
-    return (string) => {
+    let marks = Object.keys(spec)
+    return string => {
         if (string == null) {
-            return null;
+            return null
         }
-        return markdown(string, marks).map(
-            (item, index) => {
-                let name = spec[item.mark];
-                return (
-                    <span
-                        key={index}
-                        className={style[name]}>
-                        {item.text}
-                    </span>
-                )
-            }
-        );
+        return markdown(string, marks).map((item, index) => {
+            let name = spec[item.mark]
+            return (
+                <span
+                    key={index}
+                    className={style[name]}>
+                    {item.text}
+                </span>
+            )
+        })
     }
 }
 
-export default applyMarkdown;
+export default applyMarkdown
