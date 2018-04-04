b3ba4585072824f603aa507602e6d586260b7daf
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-04 18:33:10 +0200

    add omit and choose

--- src/.utils/react/react-utils.js
+++ src/.utils/react/react-utils.js
@@ -1,9 +1,31 @@
-export {
-    bind
+const get = (obj, path) => {
+    return path.split('.').reduce((acc, x) => acc && acc[x], obj)
 }
 
-function bind(context, methods) {
+export function bind(context, methods) {
     methods.forEach(item => {
-        context[item] = context[item].bind(context);
-    });
+        context[item] = context[item].bind(context)
+    })
+}
+
+// returns one case by props value
+export function choose(valuePath, cases) {
+    return props => cases[String(get(props, valuePath))]
+}
+
+// returns new props object without values
+export function omit(props, values) {
+    if (props == null) {
+        return null
+    }
+    if (values == null || !values.length) {
+        return props
+    }
+    let newProps = {}
+    Object.keys(props).forEach(prop => {
+        if (values.indexOf(prop) < 0) {
+            newProps[prop] = props[prop]
+        }
+    })
+    return newProps
 }
