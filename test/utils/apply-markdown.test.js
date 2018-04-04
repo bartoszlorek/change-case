8927af8c9a2481bef14a1d4e8f8b992dab800dc4
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-04 14:23:36 +0200

    add useStyle and clean other tests

--- test/utils/apply-markdown.test.js
+++ test/utils/apply-markdown.test.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import applyMarkdown from '../../src/.utils/react/apply-markdown'
+import applyMarkdown, { useStyle } from '../../src/.utils/react/apply-markdown'
+
+const renderFragments = frags => {
+    return renderer.create(<div>{frags}</div>).toJSON().children
+}
+
+const coolResult = [
+    'be ',
+    {
+        type: 'b',
+        props: {
+            className: 'info'
+        },
+        children: ['cool']
+    }
+]
 
 describe('apply-markdown.js', () => {
     it('should return fn that returns whole string', () => {
@@ -9,106 +24,83 @@ describe('apply-markdown.js', () => {
     })
 
     it('should handle string property', () => {
-        let mark = applyMarkdown({
-            '*': 'important'
-        })
-        let wrap = renderer.create(<div>{mark('to *be*')}</div>),
-            result = wrap.toJSON().children
+        let mark = applyMarkdown({ '*': 'info' }),
+            frag = renderFragments(mark('be *cool*'))
 
-        expect(result).toEqual([
-            'to ',
+        expect(frag).toEqual([
+            'be ',
             {
                 type: 'span',
                 props: {
-                    className: 'important'
+                    className: 'info'
                 },
-                children: ['be']
+                children: ['cool']
             }
         ])
     })
 
-    it('should handle react element property', () => {
-        let mark = applyMarkdown({
-            '*': <i />
-        })
-        let wrap = renderer.create(<div>{mark('to *be*')}</div>),
-            result = wrap.toJSON().children
-
-        expect(result).toEqual([
-            'to ',
-            {
-                type: 'i',
-                props: {},
-                children: ['be']
-            }
-        ])
+    it('should handle react element', () => {
+        let mark = applyMarkdown({ '*': <b className="info" /> }),
+            frag = renderFragments(mark('be *cool*'))
+        expect(frag).toEqual(coolResult)
     })
 
-    it('should handle functional component property', () => {
-        const Component = ({ children }) => (
-            <b className="important">{children}</b>
-        )
-        let mark = applyMarkdown({
-            '*': Component
-        })
-        let wrap = renderer.create(<div>{mark('to *be*')}</div>),
-            result = wrap.toJSON().children
-
-        expect(result).toEqual([
-            'to ',
-            {
-                type: 'b',
-                props: {
-                    className: 'important'
-                },
-                children: ['be']
-            }
-        ])
-    })
-
-    it('should handle class component property', () => {
+    it('should handle class component', () => {
         class Component extends React.Component {
             render() {
-                return <b className="important">{this.props.children}</b>
+                return <b className="info">{this.props.children}</b>
             }
         }
-        let mark = applyMarkdown({
-            '*': Component
-        })
-        let wrap = renderer.create(<div>{mark('to *be*')}</div>),
-            result = wrap.toJSON().children
-
-        expect(result).toEqual([
-            'to ',
-            {
-                type: 'b',
-                props: {
-                    className: 'important'
-                },
-                children: ['be']
-            }
-        ])
+        let mark = applyMarkdown({ '*': Component }),
+            frag = renderFragments(mark('be *cool*'))
+        expect(frag).toEqual(coolResult)
     })
 
-    it('should handle composition component property', () => {
+    it('should handle functional component', () => {
         const Component = ({ children }) => (
-            <b className="important">{children}</b>
+            <b className="info">{children}</b>
         )
-        let mark = applyMarkdown({
-            '*': <Component />
-        })
-        let wrap = renderer.create(<div>{mark('to *be*')}</div>),
-            result = wrap.toJSON().children
+        let mark = applyMarkdown({ '*': Component }),
+            frag = renderFragments(mark('be *cool*'))
+        expect(frag).toEqual(coolResult)
+    })
 
-        expect(result).toEqual([
-            'to ',
-            {
-                type: 'b',
-                props: {
-                    className: 'important'
-                },
-                children: ['be']
-            }
-        ])
+    it('should handle composition component', () => {
+        const Component = props => (
+            <b {...props}>{props.children}</b>
+        )
+        let mark = applyMarkdown({ '*': <Component className="info" /> }),
+            frag = renderFragments(mark('be *cool*'))
+        expect(frag).toEqual(coolResult)
+    })
+})
+
+describe('.useStyle', () => {
+    it('should return null', () => {
+        expect(useStyle()).toBe(null)
+        expect(useStyle(null, null)).toBe(null)
+    })
+
+    it('should return spec object', () => {
+        const spec = {
+            '*': 'info'
+        }
+        expect(useStyle(undefined, spec)).toBe(spec)
+        expect(useStyle(null, spec)).toBe(spec)
+    })
+
+    it('should transform style into spec', () => {
+        const style = {
+            'info': 'gzVnrw',
+            'warn': 'jHmeYN'
+        }
+        const spec = {
+            '*': 'info',
+            '!': 'warn'
+        }
+        expect(useStyle(style, spec)).toEqual({
+            '!': 'jHmeYN',
+            '*': 'gzVnrw'
+        })
     })
 })
