a67bd5768c57a537ca5853551fe57182bdbeea45
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-26 16:21:31 +0200

    disable message after installation

--- src/options/messages.js
+++ src/options/messages.js
@@ -5,10 +5,10 @@ import {
 } from '../.utils/chrome/extension-state'
 
 const messages = [
-    {
+    /*{
         state: INSTALL_STATE,
         value: 'Thank you. This extension is free, but if you want to support development of this or my other projects, [buy me a beer](https://www.google.com)'
-    },
+    },*/
     {
         state: UPDATE_STATE,
         value: 'The extension has been successfully updated!'
