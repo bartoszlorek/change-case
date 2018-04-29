f5caaf94ff3b4ea0d264128d0e901dcbc40f2ef2
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-29 15:59:22 +0200

    clean code and add more tests

--- src/.utils/explode.js
+++ src/.utils/explode.js
@@ -1,19 +1,23 @@
-function explode(string, points) {
+// indices should be in correct order
+// explode('hello world', [0, 2, 8])
+// => ['he', 'llo wo', 'rld']
+
+function explode(string, indices) {
     if (typeof string !== 'string') {
         return []
     }
-    let length = points == null ? 0 : points.length
+    let length = indices == null ? 0 : indices.length
     if (!length) {
         return [string]
     }
 
     let result = []
-    if (points[0] !== 0) {
-        result.push(string.substring(0, points[0]))
+    if (indices[0] !== 0) {
+        result.push(string.substring(0, indices[0]))
     }
     for (let i = 0; i < length; i++) {
-        let end = i < length ? points[i + 1] : string.length
-        result.push(string.substring(points[i], end))
+        let end = i < length ? indices[i + 1] : string.length
+        result.push(string.substring(indices[i], end))
     }
 
     return result
--- test/utils/explode.test.js
+++ test/utils/explode.test.js
@@ -2,7 +2,7 @@ import explode from '../../src/.utils/explode'
 
 const string = 'The lazy brown fox jumps over the Lazy dog.'
 
-describe('explode.js', () => {
+describe('falsy values', () => {
     it('should return an empty array', () => {
         expect(explode()).toEqual([])
         expect(explode(null)).toEqual([])
@@ -12,9 +12,21 @@ describe('explode.js', () => {
         expect(explode('dog')).toEqual(['dog'])
         expect(explode('')).toEqual([''])
     })
+})
 
-    it('should return an array with whole string', () => {
+describe('returns an array with parts of string', () => {
+    it('simple scenario', () => {
         let result = explode('lazy brown fox jumps', [4, 5, 11, 14])
         expect(result).toEqual(['lazy', ' ', 'brown ', 'fox', ' jumps'])
+    })
+
+    it('starts with zero', () => {
+        let result = explode('lazy brown fox jumps', [0, 5, 10])
+        expect(result).toEqual(['lazy ', 'brown', ' fox jumps'])
+    })
+
+    it('last index is greater than length', () => {
+        let result = explode('lazy brown fox jumps', [4, 5, 30])
+        expect(result).toEqual(['lazy', ' ', 'brown fox jumps', ''])
     })
 })
