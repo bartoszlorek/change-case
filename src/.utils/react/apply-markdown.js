cdd37c78a8149dc3bc7e551a820f5337ad1908bd
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-30 15:45:37 +0200

    use markdown prop in react component

--- src/.utils/react/apply-markdown.js
+++ src/.utils/react/apply-markdown.js
@@ -2,48 +2,46 @@ import React from 'react'
 import markdown from './markdown'
 import isReactComponent from './is-react-component'
 
+const validComponent = a => a !== null
+const PROP_TAG = '()'
+const PROP_ATTR = 'href'
+
+function baseComponent(proto, frag, key) {
+    const { text, prop } = frag
+    if (proto === undefined) {
+        return text
+    }
+    const props = { key }
+    if (prop !== null) {
+        props[PROP_ATTR] = prop
+    }
+
+    if (typeof proto === 'string') {
+        props.className = proto
+        return React.createElement('span', props, text)
+    }
+    if (isReactComponent(proto)) {
+        return React.createElement(proto, props, text)
+    }
+    if (React.isValidElement(proto)) {
+        return React.cloneElement(proto, props, text)
+    }
+    return null
+}
+
 function applyMarkdown(spec) {
     if (spec == null) {
         return string => string
     }
-
-    let marks = Object.keys(spec)
-    return (string) => {
-        if (string == null) {
-            return null
+    const marks = Object.keys(spec)
+    return string => {
+        if (string != null) {
+            return markdown(string, marks, PROP_TAG)
+                .map((frag, key) =>
+                    baseComponent(spec[frag.mark], frag, key))
+                .filter(validComponent)
         }
-        return markdown(string, marks)
-            .map((item, index) => {
-                if (item.mark === null) {
-                    return item.text
-                }
-
-                let value = spec[item.mark]
-                if (typeof value === 'string') {
-                    return React.createElement(
-                        'span', {
-                            key: index,
-                            className: value
-                        },  item.text
-                    )
-                }
-                if (isReactComponent(value)) {
-                    return React.createElement(
-                        value, {
-                            key: index
-                        },  item.text
-                    )
-                }
-                if (React.isValidElement(value)) {
-                    return React.cloneElement(
-                        value, {
-                            key: index
-                        },  item.text
-                    )
-                }
-                return false
-            })
-            .filter(a => a !== false)
+        return null
     }
 }
 
