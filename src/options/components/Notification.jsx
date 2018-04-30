e83748f77536c7b1dd07feffba211df4233facc0
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-30 15:47:21 +0200

    handle links in notification text

--- src/options/components/Notification.jsx
+++ src/options/components/Notification.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import applyMarkdown from '../../.utils/react/apply-markdown'
+
+const Link = styled.a`
+    white-space: nowrap;
+
+    &,
+    &:visited,
+    &:focus,
+    &:active,
+    &:hover {
+        color: #fff;
+        text-decoration: underline;
+    }
+    &:active,
+    &:hover {
+        text-decoration: none;
+    }
+`
+
+const mark = applyMarkdown({
+    '[]': Link
+})
 
 class Notification extends React.PureComponent {
     render() {
         return (
-            <div {...this.props}>
-                {this.props.value}
+            <div className={this.props.className}>
+                {mark(this.props.value)}
             </div>
         )
     }
+}
+
+Notification.propTypes = {
+    value: PropTypes.string
+}
+
+Notification.defaultProps = {
+    value: ''
 }
 
 export default styled(Notification)`
