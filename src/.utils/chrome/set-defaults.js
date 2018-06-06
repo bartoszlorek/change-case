07c39cb13874846f348c16b2984a8338f6fe3d32
Bartosz Lorek <bery.lorek@gmail.com>
2018-06-07 00:36:20 +0200

    set default values only after first installation

--- src/.utils/chrome/set-defaults.js
+++ src/.utils/chrome/set-defaults.js
@@ -1,19 +1,10 @@
-import { forEach } from 'lodash'
-
-function setDefaults(spec) {
-    chrome.storage.sync.get(null, data => {
-        let result = {},
-            length = 0
-
-        forEach(spec, (value, name) => {
-            if (!data.hasOwnProperty(name)) {
-                result[name] = value
-                length += 1
-            }
-        })
-
-        if (length) {
-            chrome.storage.sync.set(result)
+function setDefaults(values) {
+    if (values == null) {
+        return
+    }
+    chrome.runtime.onInstalled.addListener(event => {
+        if (event.reason === 'install') {
+            chrome.storage.sync.set(values)
         }
     })
 }
