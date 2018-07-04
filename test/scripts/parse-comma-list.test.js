7cdd1c6c1c83801693efb2faa764bf7a873c5625
Bartosz Lorek <bery.lorek@gmail.com>
2018-07-04 17:50:25 +0200

    trim and return empty array for falsy values

---
+++ test/scripts/parse-comma-list.test.js
@@ -0,0 +1,16 @@
+import parseCommaList from '../../src/scripts/parse-comma-list'
+
+it('should return empty array', () => {
+    expect(parseCommaList()).toEqual([])
+    expect(parseCommaList(null)).toEqual([])
+    expect(parseCommaList('')).toEqual([])
+    expect(parseCommaList('  ')).toEqual([])
+})
+
+it('should split comma-separated string into array', () => {
+    expect(parseCommaList('a,b,c')).toEqual(['a', 'b', 'c'])
+})
+
+it('should trim string', () => {
+    expect(parseCommaList(' a,b bb , c ')).toEqual(['a', 'b bb', 'c'])
+})
