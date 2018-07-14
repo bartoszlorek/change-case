616a1abebf82c1f6f4536ee35cac9971d59fab82
Bartosz Lorek <bery.lorek@gmail.com>
2018-07-15 00:25:26 +0200

    fix title case problem with hyphens

--- src/scripts/cases/title-case.js
+++ src/scripts/cases/title-case.js
@@ -1,3 +1,12 @@
 import toTitleCase from 'to-title-case'
+import upperCaseFirst from './.internal/upper-case-first'
 
-export default toTitleCase
+const HYPHENATED_WORDS = /[^\s]+-[^\s]+/g
+
+function titleCase(string) {
+    return toTitleCase(string).replace(HYPHENATED_WORDS, match => (
+        match.split('-').map(upperCaseFirst).join('-')
+    ))
+}
+
+export default titleCase
