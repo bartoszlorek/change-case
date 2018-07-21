cbec62f886b29f01a33f5e26f87075344ab8e44d
Bartosz Lorek <bery.lorek@gmail.com>
2018-07-21 19:37:27 +0200

    rebuild titleCase function with handling hyphens

--- src/scripts/cases/title-case.js
+++ src/scripts/cases/title-case.js
@@ -1,12 +1,27 @@
-import toTitleCase from 'to-title-case'
+import escape from 'escape-regexp-component'
+import noCase from './no-case'
+import lowerCase from './lower-case'
+import upperCase from './upper-case'
 import upperCaseFirst from './.internal/upper-case-first'
 
+import MINOR_WORDS from './.internal/minor-words'
+
+const FIRST_LETTER_REGEX = /^.|\b./gu
+const COLON_REGEX = /:\s*(\w)/g
 const HYPHENATED_WORDS = /[^\s]+-[^\s]+/g
+const MINOR_REGEX = new RegExp(
+    '\\b(' + MINOR_WORDS.map(escape).join('|') + ')\\b',
+    'igu'
+)
 
 function titleCase(string) {
-    return toTitleCase(string).replace(HYPHENATED_WORDS, match => (
-        match.split('-').map(upperCaseFirst).join('-')
-    ))
+    return noCase(string)
+        .replace(FIRST_LETTER_REGEX, upperCase)
+        .replace(MINOR_REGEX, lowerCase)
+        .replace(COLON_REGEX, upperCase)
+        .replace(HYPHENATED_WORDS, match => (
+            match.split('-').map(upperCaseFirst).join('-')
+        ))
 }
 
 export default titleCase
