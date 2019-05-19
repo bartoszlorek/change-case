8dce5b057d853714d48168d330fbc1c13bb87854
Bartosz Lorek <bartosz.lorek@brainly.com>
2019-05-20 00:13:38 +0200

    add test steps

--- src/options/tests/form.int.spec.jsx
+++ src/options/tests/form.int.spec.jsx
@@ -2,6 +2,7 @@ import '__mocks__/chrome-environment';
 import React from 'react';
 import {mount} from 'enzyme';
 import {click, change} from './test-utils';
+import {step} from './test-steps';
 
 import App from '../App';
 
@@ -20,49 +21,55 @@ describe('<App/>', () => {
     });
 
     it('should fill and save form', () => {
-      let updateNotification, ignoreList, correctList;
-
       const wrapper = mount(<App />);
 
-      updateNotification = wrapper
-        .find('[name="update-notification"]')
-        .hostNodes();
-      expect(updateNotification.prop('checked')).toBe(false);
+      step('change update notification field', () => {
+        let updateNotification = wrapper
+          .find('[name="update-notification"]')
+          .hostNodes();
+        expect(updateNotification.prop('checked')).toBe(false);
 
-      change(updateNotification, {checked: true});
+        change(updateNotification, {checked: true});
 
-      updateNotification = wrapper
-        .find('[name="update-notification"]')
-        .hostNodes();
-      expect(updateNotification.prop('checked')).toBe(true);
+        updateNotification = wrapper
+          .find('[name="update-notification"]')
+          .hostNodes();
+        expect(updateNotification.prop('checked')).toBe(true);
+      });
 
-      ignoreList = wrapper.find('[name="ignore-list"]').hostNodes();
-      expect(ignoreList.prop('value')).toBe('Hello World, John');
+      step('change ignore list field', () => {
+        let ignoreList = wrapper.find('[name="ignore-list"]').hostNodes();
+        expect(ignoreList.prop('value')).toBe('Hello World, John');
+  
+        change(ignoreList, {value: 'Hi Mark!'});
+  
+        ignoreList = wrapper.find('[name="ignore-list"]').hostNodes();
+        expect(ignoreList.prop('value')).toBe('Hi Mark!');
+      });
 
-      change(ignoreList, {value: 'Hi Mark!'});
+      step('change correct list field', () => {
+        let correctList = wrapper.find('[name="correct-list"]').hostNodes();
+        expect(correctList.prop('value')).toBe('iOS, VHS');
 
-      ignoreList = wrapper.find('[name="ignore-list"]').hostNodes();
-      expect(ignoreList.prop('value')).toBe('Hi Mark!');
+        change(correctList, {value: 'Android'});
 
-      correctList = wrapper.find('[name="correct-list"]').hostNodes();
-      expect(correctList.prop('value')).toBe('iOS, VHS');
+        correctList = wrapper.find('[name="correct-list"]').hostNodes();
+        expect(correctList.prop('value')).toBe('Android');
+      });
 
-      change(correctList, {value: 'Android'});
+      step('save form by clicking on submit', () => {
+        const submitButton = wrapper.find({value: 'Save'}).hostNodes();
 
-      correctList = wrapper.find('[name="correct-list"]').hostNodes();
-      expect(correctList.prop('value')).toBe('Android');
-
-      const submitButton = wrapper.find({value: 'Save'}).hostNodes();
-
-      click(submitButton);
-      expect(chrome.storage.sync.set).toHaveBeenLastCalledWith(
-        {
-          updateNotification: true,
-          ignoreList: 'Hi Mark!',
-          correctList: 'Android'
-        },
-        expect.any(Function)
-      );
+        click(submitButton);
+        expect(chrome.storage.sync.set).toHaveBeenLastCalledWith(
+          {
+            updateNotification: true,
+            ignoreList: 'Hi Mark!',
+            correctList: 'Android'
+          },
+          expect.any(Function)
+        );
+      });
     });
   });
 });
---
+++ src/options/tests/test-steps.js
@@ -0,0 +1,42 @@
+const originalTest = global.it;
+
+// todo: adjust indent according to parent
+const println = value => `\n        ${value}`;
+
+const passIcon = '✓';
+const failIcon = '✕';
+
+let steps = [];
+
+export const step = (description, fn) => {
+  steps.push(description);
+  fn();
+};
+
+global.it = (...args) =>  {
+  const spec = originalTest(...args);
+  const {resultCallback} = spec;
+
+  spec.resultCallback = result => {
+    const {description, status} = result;
+    const isFailed = status === 'failed';
+
+    // add steps to the test description
+    result.description = steps.reduce((output, step, index) => {
+      let icon = passIcon;
+
+      if (isFailed && index === steps.length - 1) {
+        icon = failIcon;
+      }
+
+      return output + println(`${icon} ${step}`);
+    }, description);
+
+    // call original callback
+    resultCallback(result);
+
+    steps = [];
+  };
+
+  return spec;
+};
