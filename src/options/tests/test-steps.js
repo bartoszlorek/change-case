249a563338a245754bb5d1419b2d02d4b5f6d3f5
Bartosz Lorek <bartosz.lorek@brainly.com>
2019-05-20 16:26:55 +0200

    proto should be pointing to the actual function

--- src/options/tests/test-steps.js
+++ src/options/tests/test-steps.js
@@ -1,13 +1,5 @@
-const actualTest = global.test;
-const actualIt = global.it;
-const actualDescribe = global.describe;
-
 const passIcon = '✓';
 const failIcon = '✕';
-
-const println = (value, indentLevel) => {
-  return '\n' + '  '.repeat(indentLevel || 0) + value;
-};
 
 const testIndents = {};
 let currentSteps = [];
@@ -15,67 +7,85 @@ let currentTest = null;
 
 export const step = (description, fn) => {
   if (currentTest === null) {
-    throw 'Steps can only be called inside a test.';
+    throw 'Steps can only be called inside a `test`, `it` and `fit`.';
   }
   currentSteps.push(description);
   fn();
 };
 
-const createTestProxy = func => (...args) => {
-  const spec = func(...args);
-  const actualResultCallback = spec.resultCallback;
-  const actualOnStart = spec.onStart;
+global.test = createTestProxy(global.test);
+global.it = createTestProxy(global.it);
+global.fit = createTestProxy(global.fit);
+global.describe = createDescribeProxy(global.describe);
 
-  spec.onStart = spec => {
-    currentTest = spec.id;
-    actualOnStart(spec);
-  };
+function createTestProxy(func) {
+  const proxy = (...args) => {
+    const spec = func(...args);
+    const actualResultCallback = spec.resultCallback;
+    const actualOnStart = spec.onStart;
 
-  spec.resultCallback = result => {
-    const {description, status} = result;
-    const isFailed = status === 'failed';
-
-    result.description = currentSteps.reduce((output, step, index) => {
-      let icon = passIcon;
-
-      if (isFailed && index === currentSteps.length - 1) {
-        icon = failIcon;
-      }
-
-      return output + println(`${icon} ${step}`, testIndents[spec.id] + 1);
-    }, description);
-
-    actualResultCallback(result);
-    currentSteps = [];
-    currentTest = null;
-  };
-
-  return spec;
-};
-
-global.test = createTestProxy(actualTest);
-global.it = createTestProxy(actualIt);
-
-global.describe = (...args) =>  {
-  const suite = actualDescribe(...args);
-
-  suite.children.forEach(child => {
-    if (child.constructor.name !== 'Spec') {
-      return;
-    }
-
-    let indent = 1;
-
-    const parentBubbling = suite => {
-      if (suite.parentSuite) {
-        parentBubbling(suite.parentSuite)
-        indent += 1;
-      }
+    spec.onStart = spec => {
+      currentTest = spec.id;
+      actualOnStart(spec);
     };
 
-    parentBubbling(suite);
-    testIndents[child.id] = indent;
-  });
+    spec.resultCallback = result => {
+      const {description, status} = result;
+      const isFailed = status === 'failed';
 
-  return suite;
-};
+      result.description = currentSteps.reduce((output, step, index) => {
+        let icon = passIcon;
+
+        if (isFailed && index === currentSteps.length - 1) {
+          icon = failIcon;
+        }
+
+        return output + println(`${icon} ${step}`, testIndents[spec.id] + 1);
+      }, description);
+
+      actualResultCallback(result);
+      currentSteps = [];
+      currentTest = null;
+    };
+
+    return spec;
+  };
+
+  // it should be pointing to the actual function
+  proxy.__proto__ = func;
+  return proxy;
+}
+
+function createDescribeProxy(func) {
+  const proxy = (...args) => {
+    const suite = func(...args);
+
+    suite.children.forEach(child => {
+      if (child.constructor.name !== 'Spec') {
+        return;
+      }
+
+      let indent = 1;
+
+      const parentBubbling = suite => {
+        if (suite.parentSuite) {
+          parentBubbling(suite.parentSuite)
+          indent += 1;
+        }
+      };
+
+      parentBubbling(suite);
+      testIndents[child.id] = indent;
+    });
+
+    return suite;
+  };
+
+  // it should be pointing to the actual function
+  proxy.__proto__ = func;
+  return proxy;
+}
+
+function println(value, indentLevel) {
+  return '\n' + '  '.repeat(indentLevel || 0) + value;
+}
