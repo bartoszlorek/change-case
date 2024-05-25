17be56ec353228fdedffb1d19fdc0f8a733cf27b
Bartosz Lorek <bartosz.lorek@brainly.com>
2024-05-25 08:05:20 +0200

    setup build v3

---
+++ esbuild.js
@@ -0,0 +1,71 @@
+const esbuild = require("esbuild");
+const { copy } = require("esbuild-plugin-copy");
+const { clean } = require("esbuild-plugin-clean");
+
+const [, , watchMode] = process.argv;
+
+const outdir = "dist_v3";
+const options = {
+  entryPoints: [
+    {
+      in: "src/v3/content-script/index.ts",
+      out: "content-script",
+    },
+    {
+      in: "src/v3/options-page/index.ts",
+      out: "options-page",
+    },
+    {
+      in: "src/v3/service-worker/index.ts",
+      out: "service-worker",
+    },
+  ],
+  outdir,
+  bundle: true,
+  write: true,
+  plugins: [
+    clean({
+      patterns: outdir,
+    }),
+    copy({
+      assets: [
+        { from: "assets/*", to: "assets" },
+        { from: "public/*", to: "." },
+        { from: "manifest.json", to: "manifest.json" },
+      ],
+    }),
+    logger(),
+  ],
+};
+
+if (watchMode) {
+  esbuild
+    .context({ ...options })
+    .then((ctx) => ctx.watch())
+    /**
+     * wait forever
+     * https://github.com/evanw/esbuild/issues/1885#issuecomment-1000866366
+     * https://github.com/muxinc/elements/pull/55
+     */
+    .then(() => new Promise(() => undefined));
+} else {
+  esbuild
+    .build({
+      ...options,
+      metafile: true,
+      minify: true,
+    })
+    .then((result) => esbuild.analyzeMetafile(result.metafile))
+    .then((resultInfo) => console.log(resultInfo));
+}
+
+function logger() {
+  return {
+    name: "esbuild-logger",
+    setup: (build) => {
+      build.onEnd((result) => {
+        console.log(`build ended with ${result.errors.length} errors`);
+      });
+    },
+  };
+}
