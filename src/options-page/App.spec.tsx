13b01a497aab85a9718ec5f1798db5166ceb1cbd
Bartosz Lorek <bartosz.lorek@brainly.com>
2024-09-15 21:46:15 +0200

    fix application unit tests

--- src/options-page/App.spec.tsx
+++ src/options-page/App.spec.tsx
@@ -1,34 +1,36 @@
 import * as React from 'react';
-import {render, screen, fireEvent} from '@testing-library/react';
+import {render, screen, fireEvent, waitFor} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import {App} from './App';
+
+(chrome.storage.sync.get as jest.Mock).mockResolvedValue({});
 
 describe('<App/>', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it('fills ignore list field', () => {
+  it('fills ignore list field', async () => {
     render(<App />);
 
     const textarea = screen.getByRole('textbox', {name: 'Ignore List'});
 
     fireEvent.change(textarea, {target: {value: 'foo, bar'}});
 
-    expect(textarea).toHaveValue('foo, bar');
+    await waitFor(() => expect(textarea).toHaveValue('foo, bar'));
   });
 
-  it('fills correct list field', () => {
+  it('fills correct list field', async () => {
     render(<App />);
 
     const textarea = screen.getByRole('textbox', {name: 'Correct List'});
 
     fireEvent.change(textarea, {target: {value: 'foo, bar'}});
 
-    expect(textarea).toHaveValue('foo, bar');
+    await waitFor(() => expect(textarea).toHaveValue('foo, bar'));
   });
 
-  it('saves form by clicking on submit', () => {
+  it('saves form by clicking on submit', async () => {
     render(<App />);
 
     const ignoreTextarea = screen.getByRole('textbox', {name: 'Ignore List'});
@@ -38,12 +40,11 @@ describe('<App/>', () => {
     fireEvent.change(correctTextarea, {target: {value: 'bar'}});
     fireEvent.click(screen.getByRole('button', {name: 'Save'}));
 
-    expect(chrome.storage.sync.set).toHaveBeenCalledWith(
-      {
+    await waitFor(() =>
+      expect(chrome.storage.sync.set).toHaveBeenCalledWith({
         ignoreList: 'foo',
         correctList: 'bar',
-      },
-      expect.any(Function)
+      })
     );
   });
 });
